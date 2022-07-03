3c778f87786e1ec6f98439f6840777d6b27a5d0a
Adam Turner <adamturner@Adams-MacBook-Air.local>
2022-07-03 15:57:19 +0100

    Completed algorithm challenge to find product of pairs in an array and return max pair value

---
+++ adjacentElementsProduct.js
@@ -0,0 +1,22 @@
+/*
+Given an array of integers, find the pair of adjacent elements that has the largest product 
+and return that product.
+
+Example
+
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+solution(inputArray) = 21.
+
+7 and 3 produce the largest product.
+*/
+
+function solution(inputArray) {
+    let products = [];
+    for(let i = 0; i < inputArray.length - 1; i++) {
+        products.push(inputArray[i] * inputArray[i+1]);
+    }
+    return Math.max.apply(Math, products);
+}
+
+input = [3, 6, -2, -5, 7, 3]
+
