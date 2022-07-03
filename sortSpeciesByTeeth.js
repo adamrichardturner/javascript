5a07eb3527fe8fc70184a0c3548f0cfcb73f72ac
Adam Turner <adamturner@Adams-MacBook-Air.local>
2022-07-03 06:27:57 +0100

    Function that sorts array objects by numTeeth property

--- sortSpeciesByTeeth.js
+++ sortSpeciesByTeeth.js
@@ -1,7 +1,7 @@
-const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
-
+// compareTeeth compares each array element (object) and returns whether a > b of the numTeeth property.
 const compareTeeth = (species1, species2) => species1.numTeeth > species2.numTeeth
-
+// Sorts an array of animal objects and returns in ascending order by numTeeth property.
 const sortSpeciesByTeeth = arr => arr.sort(compareTeeth)
 
+const speciesArray = [ {speciesName:'shark', numTeeth:50}, {speciesName:'dog', numTeeth:42}, {speciesName:'alligator', numTeeth:80}, {speciesName:'human', numTeeth:32}];
 console.log(sortSpeciesByTeeth(speciesArray))
