e4d525cb1166fd63b458d3f5eb337f5d28e0f52e
Adam Turner <adamturner@Adams-MacBook-Air.local>
2022-07-03 06:27:28 +0100

    Completed challenge

--- isTheDinnerVegan.js
+++ isTheDinnerVegan.js
@@ -9,4 +9,4 @@ const isTheDinnerVegan = arr => {
 const dinner = [{name: 'hamburger', source: 'meat'}, {name: 'cheese', source: 'dairy'}, {name: 'ketchup', source:'plant'}, {name: 'bun', source: 'plant'}, {name: 'dessert twinkies', source:'unknown'}];
 
 console.log(isTheDinnerVegan(dinner))
-// Should print false
+// Prints false
