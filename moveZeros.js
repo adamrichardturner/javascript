f604671b0ac1dec524b3a15a5ae3c4840fdb701e
Adam Turner <adamturner@Adams-MacBook-Air.local>
2022-07-08 18:43:40 +0100

    Completed kata to move zeros in an array to end without affecting order of other elements

---
+++ moveZeros.js
@@ -0,0 +1,11 @@
+function moveZeros(arr) {
+    /*
+    Write an algorithm that takes an array and moves all of the zeros to the end, 
+    preserving the order of the other elements.
+
+    moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+    */
+   return arr.filter(e => e !== 0).concat(arr.filter(e => e === 0));
+}
+
+console.log(moveZeros([false,1,0,1,2,0,1,3,"a"]));
