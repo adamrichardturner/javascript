0dc0843683eaf88374096fc8c933c04ded597aa5
Adam Turner <hello@adamrichardturner.dev>
2022-07-09 02:53:57 +0100

    Completed challenge

---
+++ isPangram.js
@@ -0,0 +1,23 @@
+function isPangram(string) {
+    /*
+    A pangram is a sentence that contains every single letter of the 
+    alphabet at least once. For example, the sentence "The quick brown 
+    fox jumps over the lazy dog" is a pangram, because it uses the letters 
+    A-Z at least once (case is irrelevant).
+
+    Given a string, detect whether or not it is a pangram. Return True if 
+    it is, False if not. Ignore numbers and punctuation.
+    */
+   const alpha = 'abcdefghijklmopqrstuvwxyz'.split('');
+   let alphaCount = [];
+   string.split('').forEach(i => {
+       if(alpha.includes(i)) {
+           alphaCount.push(i);
+       }
+   })
+   return [...new Set(alphaCount)].length === 25 ? true : false;
+}
+
+let x = 'The quick brown fox jumps over the lazy dog.';
+console.log(isPangram(x));
+console.log(isPangram('abcdefghijklmopqrstuvwxyz'))
