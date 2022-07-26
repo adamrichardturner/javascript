f442a476ec518fc9fbe8fa6437ef397a5d5d18cc
Adam Turner <hello@adamrichardturner.dev>
2022-07-26 10:35:48 +0100

    initial upload of kata

---
+++ removeZeros.js
@@ -0,0 +1,27 @@
+function removeZeros(array) {
+    // Sort "array" so that all elements with the value of zero are moved to the
+    // end of the array, while the other elements maintain order.
+    // [0, 1, 2, 0, 3] --> [1, 2, 3, 0, 0]
+    // Zero elements also maintain order in which they occurred.
+    // [0, "0", 1, 2, 3] --> [1, 2, 3, 0, "0"]
+    
+    // Do not use any temporary arrays or objects. Additionally, you're not able
+    // to use any Array or Object prototype methods such as .shift(), .push(), etc
+    
+    // the correctly sorted array should be returned.
+    let count = 0;
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] !== 0 && array[i]) {
+            array[count++] = array[i];
+        }
+    }
+    console.log(array)
+    console.log(array[2])
+    while(count < array.length) {
+        array[count++] = 0;
+    }
+    return array;
+}
+
+// console.log(removeZeros([0, 1, 2, 0, 3])); // [1, 2, 3, 0, 0]
+console.log(removeZeros([0, "0", 1, 2, 3])); // [1, 2, 3, 0, "0"]
