edeee94716d0727382a205ddf0e602949dc7f589
Adam Turner <hello@adamrichardturner.dev>
2022-08-04 18:42:39 +0100

    Completed kata to count pairs of gloves in array

---
+++ numberOfPairs.js
@@ -0,0 +1,36 @@
+function numberOfPairs(gloves) {
+    /*
+    Winter is coming, you must prepare your ski holidays. 
+    The objective of this kata is to determine the number 
+    of pair of gloves you can constitute from the gloves 
+    you have in your drawer.
+
+    Given an array describing the color of each glove, 
+    return the number of pairs you can constitute, assuming 
+    that only gloves of the same color can form pairs.
+
+    Examples:
+    input = ["red", "green", "red", "blue", "blue"]
+    result = 2 (1 red pair + 1 blue pair)
+
+    input = ["red", "red", "red", "red", "red", "red"]
+    result = 3 (3 red pairs)
+    */
+   // Store in colours the individual colours of the gloves...
+   const colours = [...new Set(gloves)];
+   // Store in allGloves an object representation of the values in colours
+   const allGloves = colours.reduce((a, b) => {
+       return {...a, [b]: 0}
+   }, {});
+   // For each glove in gloves, if the glove is present in allGloves, increment it's value
+   // This gives us a count of the number of gloves by colour...
+   gloves.forEach(glove => glove in allGloves ? allGloves[glove] += 1 : allGloves[glove] += 0);
+   // Following validation, return the values of allGloves / 2 to get pairs in floating point notation, Floor divide and sum
+   // up to get the total number of valid pairs
+   return (gloves.length > 0 ? Object.values(allGloves).map(n => n / 2).map(n => Math.floor(n)) : 0).reduce((a, b) => a + b);
+}
+
+console.log(numberOfPairs(['red','red'])); // 1
+console.log(numberOfPairs(['red','green','blue'])); // 0
+console.log(numberOfPairs(["red", "green", "red", "blue", "blue"])); // 2
+console.log(numberOfPairs(["red", "red", "red", "red", "red", "red"])); // 3
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,13 +1,4 @@
-function position(letter) {
-    /*
-    When provided with a letter, return its position in the alphabet.
-    
-    Input :: "a"
-
-    Ouput :: "Position of alphabet: 1"
-    */
-    let position = 'abcdefghijklmnopqrstuvwxyz'.indexOf(letter) + 1;
-    return `Position of alphabet: ${position}`;
-}
-
-console.log(position('z')); // Position of alphabet: 26
+console.log(numberOfPairs(['red','red']));
+// console.log(numberOfPairs(['red','green','blue']));
+// console.log(numberOfPairs(["red", "green", "red", "blue", "blue"]));
+// console.log(numberOfPairs(["red", "red", "red", "red", "red", "red"]));
