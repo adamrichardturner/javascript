012072f36f49a604ae02accd7196cc7388f852c4
Adam Turner <hello@adamrichardturner.dev>
2022-08-04 17:24:51 +0100

    Completed kata to double every other integer in array

---
+++ doubleEveryOther.js
@@ -0,0 +1,17 @@
+function doubleEveryOther(a) {
+    /*
+    Write a function that doubles every second integer in a list, starting from the left.
+
+    Example:
+
+    For input array/list :
+
+    [1,2,3,4]
+    the function should return :
+
+    [1,4,3,8]
+    */
+   return a.map((e, i) => i % 2 != 0 ? e * 2 : e);
+}
+
+console.log(doubleEveryOther([1,2,3,4]));
