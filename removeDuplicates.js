bb6be916baa0e80849a7a4b47d42281ab400ae28
Adam Turner <hello@adamrichardturner.dev>
2022-08-31 20:07:14 +0100

    Edited comments and formatted code

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -1,5 +1,5 @@
 function solve(arr) {
-    /*
+  /*
     Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
 
     Example:
@@ -11,23 +11,22 @@ function solve(arr) {
     Expected output: [4, 6, 3]
 
     More examples can be found in the test cases.
-    */
+  */
 
-   // Store in unique our unique values
-   let unique = [];
-   // For each element in arr from the end...
-   for(let i = arr.length-1; i >= 0; i--) {
+  // Store in unique our unique values
+  let unique = [];
+  // For each element in arr from the end...
+  for (let i = arr.length - 1; i >= 0; i--) {
     // If unique includes that element, continue...
-    if(unique.includes(arr[i])) {
-        continue;
+    if (unique.includes(arr[i])) {
+      continue;
     } else {
-        // else add that unique value to unique
-        unique.push(arr[i]);
+      // else add that unique value to unique
+      unique.push(arr[i]);
     }
-
-   }
-   // Reeturn the unique values reversed, as we added them in reverse
-   return unique.reverse();
+  }
+  // Return the unique values reversed, as we added them in reverse
+  return unique.reverse();
 }
 
 console.log(solve([3, 4, 4, 3, 6, 3])); // [ 4, 6, 3 ]
