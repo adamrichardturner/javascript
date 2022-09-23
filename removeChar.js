8550649285d63f25178df05abf9dbeab2a22b877
Adam Turner <hello@adamrichardturner.dev>
2022-09-23 22:51:44 +0100

    Completed kata

---
+++ removeChar.js
@@ -0,0 +1,10 @@
+function removeChar(str) {
+    /*
+    It's pretty straightforward. Your goal is to create a function that removes the 
+    first and last characters of a string. You're given one parameter, the original 
+    string. You don't have to worry with strings with less than two characters.
+    */
+    return str.slice(1, str.length-1);
+};
+
+console.log(removeChar('eloquent')); // loquen
