1d2ceb052fa010e5a05ae5f1480224e9c8d73d4e
Adam Turner <hello@adamrichardturner.dev>
2022-10-06 22:53:50 +0100

    Completed kata

---
+++ addBinary.js
@@ -0,0 +1,18 @@
+function addBinary(a,b) {
+    /*
+    Implement a function that adds two numbers together and returns their sum in binary. 
+    The conversion can be done before, or after the addition.
+
+    The binary number returned should be a string.
+
+    Examples:(Input1, Input2 --> Output (explanation)))
+
+    1, 1 --> "10" (1 + 1 = 2 in decimal or 10 in binary)
+    5, 9 --> "1110" (5 + 9 = 14 in decimal or 1110 in binary)
+    */
+
+    // Add the numbers first as integers, then convert to binary
+    return (a + b).toString(2);
+}
+
+console.log(addBinary(1, 1)); // '10'
--- reverseEveryOther.js
+++ reverseEveryOther.js
@@ -6,11 +6,11 @@ function reverse(str) {
     treated as if they are a part of the word in this kata.
     */
 
-    // Trim string for whitespace, split and map - on each indice which is not even
-    // split, reverse and join the word, otherwise just return the word, join the array 
+    // Trim string for whitespace, split and map - on each index which is not even
+    // split, reverse and join the word, otherwise just return the word. Then join the array 
     // as a string and return it...
     return str.trim().split(' ').map((e, i) => i % 2 != 0 ? e.split('').reverse().join('') : e).join(' ');
 }
 
-console.log(reverse("Reverse this string, please!"));
-console.log(reverse("I really don't like reversing strings!"));
+console.log(reverse("Reverse this string, please!")); // Reverse siht string, !esaelp
+console.log(reverse("I really don't like reversing strings!")); // I yllaer don't ekil reversing !sgnirts
