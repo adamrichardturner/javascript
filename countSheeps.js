daba87b2c108e7f4b6ad5f9c01221c5f5c0378aa
Adam Turner <hello@adamrichardturner.dev>
2023-03-10 17:24:39 +0000

    Completed kata

--- countSheeps.js
+++ countSheeps.js
@@ -17,4 +17,9 @@ function countSheeps(arrayOfSheep) {
     return arrayOfSheep.filter(sheep => sheep === true).length
 }
 
-countSheeps([])
+countSheeps([true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true]); // 17
