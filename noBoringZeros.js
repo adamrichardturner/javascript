8cb5cce6b9b384a1adee5cbd6bbee3be48df657a
adamrichardturner <hello@adamrichardturner.dev>
2023-07-15 23:56:36 +0100

    Added comments

--- noBoringZeros.js
+++ noBoringZeros.js
@@ -3,15 +3,19 @@ function noBoringZeros(n) {
   // They might be fun in your world, but not here.
   // Get rid of them. Only the ending ones.
 
+  // First check if n is negative
   let isNeg = n < 0
+  // If negative, convert to positive
   if (isNeg) n *= -1
-
+  // Store in filtered n with the leading zeros removed
   const filtered = Number(
     String(Number(n.toString().split('').reverse().join('')))
       .split('')
       .reverse()
       .join('')
   )
+  // If n was negative at start,
+  // flip back to a negative result
   return isNeg ? filtered * -1 : filtered
 }
 
